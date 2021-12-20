ea6fd1a3ed920ffd284e1a5aae33a85ead18bab7
Tabata Caroline Souto <93151350+TabataSouto@users.noreply.github.com>
2021-12-20 19:51:14 -0300

    Update script.js
    Incremento de pokemons aleatórios

--- script.js
+++ script.js
@@ -1,43 +1,79 @@
 // constantes globais;
-const start = document.querySelector('#start');
+const imgPokemon = document.querySelector('#pokemon')
+const input = document.querySelector('#answer');
+const nextRestar = document.querySelector('#restart');
+const namePokemon = document.querySelector('#text');
 const buttonStart = document.querySelector('#button');
+const startGame = document.querySelector('#start');
 const game = document.querySelector('#game');
-const answers = document.querySelector('#answers');
-const restart = document.querySelector('#restart');
 
-// iniciar o jogo;
-start.addEventListener('click', () => {
+// iniciar o jogo
+startGame.addEventListener('click', () => {
   buttonStart.style.display = 'none';
   game.style.display = 'block';
-});
+})
 
-// items do jogo;
-// ans1, ans2 e ans3 são as opções de respostas com o nome de pokemons;
-// name é o nome do pokemon da imagem;
-// imagem do pokemon com sombra
-function addItems(ans1, ans2, ans3, name, img) {
-  answer1.innerText = ans1;
-  answer2.innerText = ans2;
-  answer3.innerText = ans3;
-  text.innerText = name;
-  pokemon.src = img;
+// pokemon aleatorio
+function randomPokemon() {
+  const randomImg = Math.round(Math.random() * 9);
+  const img = pokemons[randomImg].shadow;
+  const name = pokemons[randomImg].name;
+  imgPokemon.src = img;
+  namePokemon.innerText = name;
 }
-addItems('Pikachu', 'Zubat', 'Wanriky', 'Pikachu', 'images/pikachu-sombra.png');
+randomPokemon();
 
-// jogo;
-answers.addEventListener('click', selectedAnswer);
+// envio de resposta;
+const sendButton = document.querySelector('#send');
 
-function selectedAnswer(event, img){
-  const click = event.target.innerText;
-  const pokemon = document.querySelector('#pokemon');
-  const correctAnswer = document.querySelector('#text');
-  if (click === correctAnswer.innerText) {
-    pokemon.src = './images/pikachu.png';
-    restart.style.display = 'block';
+sendButton.addEventListener('click', () => {
+  if (input.value === 'Eevee') {;
+    imgPokemon.src = pokemons[0].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-}
+  else if (input.value === 'Chimchar') {;
+    imgPokemon.src = pokemons[1].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else if (input.value === 'Entei') {;
+    imgPokemon.src = pokemons[2].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  } 
+  else if (input.value === 'Gardevoir') {;
+    imgPokemon.src = pokemons[3].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else if (input.value === 'Minun') {;
+    imgPokemon.src = pokemons[4].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else if (input.value === 'Teddiursa') {;
+    imgPokemon.src = pokemons[5].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else if (input.value === 'Bayleef') {;
+    imgPokemon.src = pokemons[6].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else if (input.value === 'Palkia') {;
+    imgPokemon.src = pokemons[7].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else if (input.value === 'Psyduck') {;
+    imgPokemon.src = pokemons[8].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else if (input.value === 'Riolu') {;
+    imgPokemon.src = pokemons[9].img;
+    restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
+  }
+  else {
+    restart.innerText = 'Errado! Tente novamente';
+  }
+})
 
-// próximo pokemon;
-// restart.addEventListener('click', () => {
-//   addItems('Pikachu', 'Zubat', 'Eve', 'Pikachu', 'images/eve-sombra.jpg');
-// });
+nextRestar.addEventListener('click', () => {
+  randomPokemon();
+  restart.innerText = 'Então, qual é o Pokemon?';
+  input.value = '';
+});
