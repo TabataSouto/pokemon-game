809bb8d2512785c84f1933b41a2fb87b175c32d1
Tabata Caroline Souto <93151350+TabataSouto@users.noreply.github.com>
2021-12-20 19:52:08 -0300

    Create script-pokemons.js
    Objeto com pokemons para gerar aleatoriamente

---
+++ script-pokemons.js
@@ -0,0 +1,51 @@
+const pokemons = [{
+        name: 'Eevee',
+        img: '/images/eevee.png',
+        shadow: '/images/eevee-sombra.png',
+    },
+    {
+        name: 'Chimchar',
+        img: '/images/chimchar.png',
+        shadow: '/images/chimchar-sombra.png',
+    },
+    {
+        name: 'Entei',
+        img: '/images/entei.png',
+        shadow: '/images/entei-sombras.png',
+    },
+    {
+        name: 'Gardevoir',
+        img: '/images/gardevoir.png',
+        shadow: '/images/gardevoir-sombra.png',
+    },
+    {
+        name: 'Minun',
+        img: '/images/minun.png',
+        shadow: '/images/minun-sombra.png',
+    },
+    {
+        name: 'Teddiursa',
+        img: '/images/teddiursa.png',
+        shadow: '/images/teddiursa-sombra.png',
+    },
+    {
+        name: 'Bayleef',
+        img: '/images/bayleef.png',
+        shadow: '/images/bayleef-sombra.png',
+    },
+    {
+        name: 'Palkia',
+        img: '/images/palkia.png',
+        shadow: '/images/palkia-sombra.png',
+    },
+    {
+        name: 'Psyduck',
+        img: '/images/psyduck.png',
+        shadow: '/images/psyduck-sombra.png',
+    },
+    {
+        name: 'Riolu',
+        img: '/images/riolu.png',
+        shadow: '/images/riolu-sombra.png',
+    },
+];
