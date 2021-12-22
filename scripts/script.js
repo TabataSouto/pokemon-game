38df4208512f9aa1e5be02d891c5d965d599c0f1
Tabata <tabatac.souto@gmail.com>
2021-12-22 12:36:34 -0300

    mudando para 1 javascript apenas

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,62 @@
+// objeto com pokemons
+const pokemons = [{
+  name: 'Eevee',
+  img: '/images/eevee.png',
+  shadow: '/images/eevee-sombra.png',
+},
+{
+  name: 'Chimchar',
+  img: '/images/chimchar.png',
+  shadow: '/images/chimchar-sombra.png',
+},
+{
+  name: 'Entei',
+  img: '/images/entei.png',
+  shadow: '/images/entei-sombras.png',
+},
+{
+  name: 'Gardevoir',
+  img: '/images/gardevoir.png',
+  shadow: '/images/gardevoir-sombra.png',
+},
+{
+  name: 'Minun',
+  img: '/images/minun.png',
+  shadow: '/images/minun-sombra.png',
+},
+{
+  name: 'Teddiursa',
+  img: '/images/teddiursa.png',
+  shadow: '/images/teddiursa-sombra.png',
+},
+{
+  name: 'Bayleef',
+  img: '/images/bayleef.png',
+  shadow: '/images/bayleef-sombra.png',
+},
+{
+  name: 'Palkia',
+  img: '/images/palkia.png',
+  shadow: '/images/palkia-sombra.png',
+},
+{
+  name: 'Psyduck',
+  img: '/images/psyduck.png',
+  shadow: '/images/psyduck-sombra.png',
+},
+{
+  name: 'Riolu',
+  img: '/images/riolu.png',
+  shadow: '/images/riolu-sombra.png',
+},
+{
+  name: 'Pokemon Chorando',
+  img: '/images/pokemon-cry.png',
+},
+];
+
+// -----------------------------------------------------------------
+
 // constantes globais;
 const imgPokemon = document.querySelector('#pokemon')
 const input = document.querySelector('#answer');
@@ -28,53 +87,58 @@ randomPokemon();
 const sendButton = document.querySelector('#send');
 
 sendButton.addEventListener('click', () => {
-  if (input.value === 'Eevee') {;
+  if (input.value === (pokemons[0].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[0].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Chimchar') {;
+  else if (input.value === (pokemons[1].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[1].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Entei') {;
+  else if (input.value === (pokemons[2].name).toLowerCase()) {;
     imgPokemon.src = pokemons[2].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   } 
-  else if (input.value === 'Gardevoir') {;
+  else if (input.value === (pokemons[3].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[3].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Minun') {;
+  else if (input.value === (pokemons[4].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[4].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Teddiursa') {;
+  else if (input.value === (pokemons[5].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[5].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Bayleef') {;
+  else if (input.value === (pokemons[6].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[6].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Palkia') {;
+  else if (input.value === (pokemons[7].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[7].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Psyduck') {;
+  else if (input.value === (pokemons[8].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[8].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
-  else if (input.value === 'Riolu') {;
+  else if (input.value === (pokemons[9].name).toLowerCase().toUpperCase()) {;
     imgPokemon.src = pokemons[9].img;
     restart.innerText = 'Parabéns! Vamos para o próximo Pokemon?';
   }
   else {
+    imgPokemon.src = pokemons[10].img;
+    restart.style.color = 'red';
+    restart.style.fontWeight = '800';
     restart.innerText = 'Errado! Tente novamente';
   }
 })
 
 nextRestar.addEventListener('click', () => {
   randomPokemon();
+  restart.style.color = 'black';
+  restart.style.fontWeight = '0';
   restart.innerText = 'Então, qual é o Pokemon?';
   input.value = '';
 });
