c6c0df97cb3675dc1ee2f95c596336df83e1e6fa
Tabata <tabatac.souto@gmail.com>
2022-01-03 10:48:11 -0300

    adiçao de 1 pokemon para acerto

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,25 @@
+// ------------- Botão Go para iniciar o jogo ------------
 const startGame = document.getElementById('start-game');
 const sectionGame = document.getElementById('game');
 
 startGame.addEventListener('click', () => {
   startGame.style.display = 'none';
   sectionGame.style.display = 'block';
+})
+
+// ------------- Adição da imagem sombra ------------
+function addShadow() {
+  const pokemon = document.querySelector('#pokemon');
+  pokemon.src = '/images/minun-sombra.png'
+}
+addShadow();
+
+// ------------- Acertou ou Errou -----------------
+const answer = document.querySelector('#answer');
+const send = document.querySelector('#send');
+
+send.addEventListener('click', () => {
+  if (answer.value === 'Minun'.toLowerCase()) {
+    pokemon.src = '/images/minun.png';
+  }
 })
