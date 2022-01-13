e150a8188bdabd145c484edc7ca492ee4e71aa24
Tabata <tabatac.souto@gmail.com>
2022-01-13 14:35:12 -0300

    Retorno de acertou ou erro do pokemon: finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 // ---------------- constantes globais -------------------
 const startGame = document.querySelector('#start-game');
 const sectionGame = document.querySelector('#game');
-const pokemon = document.querySelector('#pokemon');
-const answer = document.querySelector('#answer');
-const send = document.querySelector('#send');
-const restartNext = document.querySelector('#restart');
-const textNamePokemon = document.querySelector('#text')
+const pokemonImg = document.querySelector('#pokemon');
+const titlePokemon = document.querySelector('#text')
+const inputAnswer = document.querySelector('#answer');
+const sendButton = document.querySelector('#send');
+const restartNextButton = document.querySelector('#restart');
 
 // Botão "Go" para iniciar o jogo
 startGame.addEventListener('click', () => {
@@ -16,20 +16,30 @@ startGame.addEventListener('click', () => {
 // Imagem de sombra do jogo
 function addShadow() {
   const randomPokemon = Math.round(Math.random() * 9);
-  pokemon.src = pokemons[randomPokemon].shadow;
-  textNamePokemon.innerText = pokemons[randomPokemon].name;
+  pokemonImg.src = pokemons[randomPokemon].shadow;
+  titlePokemon.innerText = pokemons[randomPokemon].name
 }
 addShadow();
 
+console.log(titlePokemon.innerText);
+
 //Função que retorna o resultado do acerto e erro;
 send.addEventListener('click', () => {
-  // refazer
+  if (inputAnswer.value === titlePokemon.innerText.toLowerCase()) {
+    const image = pokemons.find((pokemon) => 
+      titlePokemon.innerText === pokemon.name);
+    pokemonImg.src = image.img;
+    restartNextButton.innerText = 'Parabéns, você acertou! Vamos para o próximo Pokémon?'
+  } else {
+    restartNextButton.style.color = 'rgb(209, 19, 19)';
+    restartNextButton.innerText = 'Errado! Vamos tentar novamente?';
+  }
 });
 
 // Tentar outros pokemons
-restartNext.addEventListener('click', () => {
-  answer.value = '';
-  restartNext.innerText = 'Então, qual é o Pokémon?'
-  restartNext.style.color = 'rgb(27, 65, 136)';
+restartNextButton.addEventListener('click', () => {
+  inputAnswer.value = '';
+  restartNextButton.innerText = 'Então, qual é o Pokémon?'
+  restartNextButton.style.color = 'rgb(27, 65, 136)';
   addShadow();
 });
